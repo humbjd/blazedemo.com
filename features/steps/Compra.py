b93d689e4e5a5e426904cd608ac72c217fde422e
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-03-02 11:32:10 -0300

    Alimentacão dos dados no behave

--- features/steps/Compra.py
+++ features/steps/Compra.py
@@ -1,56 +1,60 @@
 from behave import given, when, then
 
-
 @given(u'que acesso o site Blazedemo')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given que acesso o site Blazedemo')
+
+    print('Passo 1 - Acessou o site Blazedemo')
 
 
 @when(u'seleciono a a cidade de origem como "São Paolo"')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When seleciono a a cidade de origem como "São Paolo"')
+
+    print('Passo 2 - Selecionou a cidade de origem')
 
 
 @when(u'seleciono a cidade de destino como "Rome"')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When seleciono a cidade de destino como "Rome"')
+
+    print('Passo 3 - Selecionou a cidade de destino')
 
 
 @when(u'clico no botao Find Flights')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When clico no botao Find Flights')
+
+    print('Passo 4 - Clicou no botão Find Flights')
 
 
 @then(u'sou direcionado para a pagina de selecao de voos')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then sou direcionado para a pagina de selecao de voos')
 
+    print('Passo 5 - Direcionou para a pagina de pagamento')
 
 @when(u'seleciono o primeiro voo')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When seleciono o primeiro voo')
 
+    print('Passo 6 - Selecionou o primeiro voo')
 
 @then(u'sou direcionado para a pagina de pagamento')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then sou direcionado para a pagina de pagamento')
 
+    print('Passo 7 - Direcionou para a pagina de pagamento')
 
 @when(u'preencho os dados para o pagamento')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When preencho os dados para o pagamento')
 
+    print('Passo 8 - Preencheu os dados para o pagamento ')
 
 @when(u'clico no botao Puchase Flight')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When clico no botao Puchase Flight')
 
+    print('Passo 9 - Clicou no botão Purchase Flight')
 
 @then(u'sou direcionado para a pagina de confirmacao')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then sou direcionado para a pagina de confirmacao')
 
+    print('Passo 10 - Direcionou para a pagina de confirmação')
 
 @when(u'seleciono a a cidade de origem como "São Paolo" para "Rome"')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When seleciono a a cidade de origem como "São Paolo" para "Rome"')
+
+    print('Passo 2c - Seleciocou a cidade de origem e destino')
