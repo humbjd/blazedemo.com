37421af9c457b22c1700e4b282a5c1413e57d692
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-03-02 10:56:18 -0300

    Criação do behave

---
+++ features/steps/Compra.py
@@ -0,0 +1,56 @@
+from behave import given, when, then
+
+
+@given(u'que acesso o site Blazedemo')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given que acesso o site Blazedemo')
+
+
+@when(u'seleciono a a cidade de origem como "São Paolo"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When seleciono a a cidade de origem como "São Paolo"')
+
+
+@when(u'seleciono a cidade de destino como "Rome"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When seleciono a cidade de destino como "Rome"')
+
+
+@when(u'clico no botao Find Flights')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When clico no botao Find Flights')
+
+
+@then(u'sou direcionado para a pagina de selecao de voos')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then sou direcionado para a pagina de selecao de voos')
+
+
+@when(u'seleciono o primeiro voo')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When seleciono o primeiro voo')
+
+
+@then(u'sou direcionado para a pagina de pagamento')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then sou direcionado para a pagina de pagamento')
+
+
+@when(u'preencho os dados para o pagamento')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When preencho os dados para o pagamento')
+
+
+@when(u'clico no botao Puchase Flight')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When clico no botao Puchase Flight')
+
+
+@then(u'sou direcionado para a pagina de confirmacao')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then sou direcionado para a pagina de confirmacao')
+
+
+@when(u'seleciono a a cidade de origem como "São Paolo" para "Rome"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When seleciono a a cidade de origem como "São Paolo" para "Rome"')
