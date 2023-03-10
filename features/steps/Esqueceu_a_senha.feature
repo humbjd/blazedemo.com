47c538424554e13b1fb2f6e9d0d766ccb1be6df4
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-03-10 14:25:36 -0300

    Correção da feature Esqueceu_a_senha.feature

--- features/steps/Esqueceu_a_senha.feature
+++ features/steps/Esqueceu_a_senha.feature
@@ -1,11 +1,13 @@
 @esqueceu_a_senha
 Feature: Esqueceu a senha
+
   Scenario Outline: Usuarios com Cadastro
     Given que acesso o site Blazedemo
     When clico em home
-    And clico no Link Forgot Your Password?
+    And clico no link Forgot Your Password?
     Then vejo a pagina de reiniciar a senha
     When preencho meu "<email>" e clico no botao Send Password Reset Link
+    Then vejo a mensagem de confirmacao
     Examples:
       | email                   |
       | juca@iterasys.com.br    |
--- features/steps/Esqueceu_a_senha.py
+++ features/steps/Esqueceu_a_senha.py
@@ -1,20 +1,17 @@
-from behave import when,then
+from behave import when, then
+from selenium.webdriver.common.by import By
 
 
-@when(u'clico no Link Forgot Your Password?')
+@when(u'clico no link Forgot Your Password?')
 def step_impl(context):
-
-
+    context.driver.find_element(By.LINK_TEXT, 'Forgot Your Password?').click()
 
 @then(u'vejo a pagina de reiniciar a senha')
 def step_impl(context):
+    assert context.driver.find_element(By.CSS_SELECTOR, 'div.panel-heading').text == 'Reset Password'
 
+@when(u'preencho meu "{email}" e clico no botao Send Password Reset Link')
+def step_impl(context, email):
+    context.driver.find_element(By.ID, 'email').send_keys(email)
+    context.driver.find_element(By.CSS_SELECTOR, 'button.btn.btn-primary').click()
 
-
-@when(u'preencho meu "juca@iterasys.com.br" e clico no botao Send Password Reset Link')
-def step_impl(context):
-
-
-
-@when(u'preencho meu "correia@iterasys.com.br" e clico no botao Send Password Reset Link')
-def step_impl(context):
